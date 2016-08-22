0b492b3fd0defb8d536fad1eea30b783e0114f0d
Guy <guy@sentinelone.com>
2016-08-22 04:13:46 +0300

    Basic Bernstein, fixed a few typos in Fnv

---
+++ idaplugin/rematch/collectors/BernsteinHash.py
@@ -0,0 +1,49 @@
+from . import Vector 
+from .. import exceptions
+import idaapi
+
+
+class BernsteinVec(Vector):
+
+  # TODO refactor 
+  # We have many hash functions which do the same
+  # just create a class to iterate over it and implement digest
+  def collect_insn(self,fn):
+   lst = []
+   fn_lst = []
+   for bb in fn:
+    for insn in bb.succs():
+      start = bb.startEA
+      end = bb.endEA
+      while start < end:
+        insn_len =  idaapi.decode_insn(start)
+        indx = 0
+        while indx < insn_len:
+          lst.append(Byte(start +  indx))
+          indx +=1
+        start = NextHead(start)
+      fn_lst.append(digest(lst))
+      lst = []
+    return fn_lst
+
+  # this is the "modified better" version 
+  # of the infamous bernstein hash 
+  # http://www.eternallyconfuzzled.com/tuts/algorithms/jsw_tut_hashing.aspx
+  # not safe at all for crytographic hashes, but one of the first
+  # hashes by djb.
+  # TODO XXX add more transformations 
+  def digest(self,bytearr):
+    r = 0
+    for b in bytearr:
+      r = 33 * r 
+      r = r ^ b
+    return r
+
+  # ugly 
+  def collect(self,ea):
+    fn = idaapi.get_func(ea)
+    if None == fn:
+      raise exceptions.NoFunctionException()
+    return collect_insn(fn)
+
+
--- idaplugin/rematch/collectors/FnvHash.py
+++ idaplugin/rematch/collectors/FnvHash.py
@@ -1,29 +1,23 @@
 from . import Vector 
 from .. import exceptions
 import idaapi
-import ctypes
 
 # we only do 64b-len hashes, not 32b
 # TODO XXX create a native function to do this hash
 class FnvHash:
+
   FNV_P = 0x100000001b3
   FNV_OFF_BASE = 0xcbf29ce484222325
   BITNESS = 2**64
 
 class FnvHashVector(Vector):
 
-  def collect(self,ea):
-    fn = idaapi.get_func(ea)
-    if None == fn:
-      raise exceptions.NoFunctionException()
-    collect_insn(fn)
-
   # TODO XXX multiplex.
   # Move from Byte to Dword.
   #  For every basic block 
   # calculate FNV Hash.
   # then add it to a global lst
-  def collect_insn(fn):
+  def collect_insn(self,fn):
     lst = []
     fn_lst = []
     for basicblock in fn:
@@ -34,17 +28,24 @@ class FnvHashVector(Vector):
           insn_len = idaapi.decode_insn(bb_start)
           indx = 0
           while indx < insn_len:
-            lst.append(Byte(start + indx))
+            lst.append(Byte(bb_start + indx))
             indx +=1
           bb_start = NextHead(bb_start)
         fn_lst.append(digest(lst))
         lst = [] 
     return fn_lst
 
-  def digest(self,bytearr):
+  def digest(self,bytearr): 
     h = FnvHash.FNV_OFF_BASE
     for b in bytearr:
       h = h ^ b
       h = (h * FnvHash.FNV_P) % FnvHash.BITNESS
     return h
+
+  # ugly 
+  def collect(self,ea): 
+    fn = idaapi.get_func(ea)
+    if None == fn:
+      raise exceptions.NoFunctionException()
+    return collect_insn(fn)
 
