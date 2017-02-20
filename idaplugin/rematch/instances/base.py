ddcc94de25a69b660a9ed4e7fa1253153aaef472
Nir Izraeli <nirizr@gmail.com>
2017-02-20 19:45:52 +0200

    Cleaner set aggregation of vectors and annotations
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- idaplugin/rematch/instances/base.py
+++ idaplugin/rematch/instances/base.py
@@ -5,11 +5,8 @@ class BaseInstance(object):
   def __init__(self, file_version, offset):
     self.file_version = file_version
     self.offset = offset
-    self.vectors = set()
-    self.annotations = set()
-
-    self.vectors.add(collectors.NameHashVector)
-    self.annotations.add(collectors.NameAnnotation)
+    self.vectors = {collectors.NameHashVector}
+    self.annotations = {collectors.NameAnnotation}
 
   def serialize(self):
     vectors = list(collectors.collect(self.offset, self.vectors))
