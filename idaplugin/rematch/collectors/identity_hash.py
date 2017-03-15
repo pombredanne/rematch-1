74d58bb73877f01ed31438946de4d26e4b6193b0
Nir Izraeli <nirizr@gmail.com>
2017-03-15 02:41:12 +0200

    Add identity hash vector
    This is an old matcher from the original rematch project.
    This aims to generate a unique hash for a function based on its exact binary
    instructions, only ignoring reference offsets (as those might change based on
    the function's location in the file even for exact copies).
    
    Reference offsets are ignored by only taking the first instruction byte, by
    operating under the assumption the first byte holds the type of reference
    (call, jump, conditional jump) and subsequent bytes the actual offsets.
    
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

---
+++ idaplugin/rematch/collectors/identity_hash.py
@@ -0,0 +1,29 @@
+import idautils
+import idc
+
+from .vector import Vector
+
+
+class IdentityHashVector(Vector):
+  type = 'identity_hash'
+  type_version = 0
+
+  def __init__(self, **kwargs):
+    super(IdentityHashVector, self).__init__(**kwargs)
+    self.hash = 17391172068829961267
+
+  def _cycle(self, b):
+    self.hash |= 5
+    self.hash ^= b
+    self.hash *= self.hash
+    self.hash ^= (self.hash >> 32)
+    self.hash &= 0xffffffffffffffff
+
+  def _data(self):
+    for ea in idautils.FuncItems(self.offset):
+      self._cycle(idc.Byte(ea))
+      if idc.isRef(idc.GetFlags(ea)):
+        continue
+      for i in range(ea + 1, ea + idc.ItemSize(ea)):
+        self._cycle(idc.Byte(i))
+    return self.hash
