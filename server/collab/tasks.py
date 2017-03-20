453483289/rematch

Add a matchers Task field and only use listed matchers when matching
Signed-off-by: Nir Izraeli <nirizr@gmail.com>

server/collab/tasks.py
@@ -5,24 +5,29 @@ from collab import matches
 
 from celery import shared_task
 
+import json
+
 
 @shared_task
 def match(task_id):
   try:
-    # recording the task has started
     task = Task.objects.filter(id=task_id)
-    task.update(status=Task.STATUS_STARTED, progress=0,
-                progress_max=len(matches.match_list),
-                task_id=match.request.id)
 
     # get input parameters
     task_values = task.values_list('id', 'source_file_version__file_id',
                                    'source_start', 'source_end',
                                    'source_file_version_id',
-                                   'target_project_id', 'target_file_id').get()
+                                   'target_project_id', 'target_file_id',
+                                   'matchers').get()
     print(task_values)
     (task_id, source_file, source_start, source_end, source_file_version,
-     target_project, target_file) = task_values
+     target_project, target_file, requested_matchers) = task_values
+
+    requested_matchers = set(json.loads(requested_matchers))
+
+    # recording the task has started
+    task.update(status=Task.STATUS_STARTED, progress=0,
+                progress_max=len(requested_matchers), task_id=match.request.id)
 
     source_filter = {'file_id': source_file,
                      'file_version_id': source_file_version}
@@ -43,6 +48,10 @@ def match(task_id):
     print("Running task {}".format(match.request.id))
     # TODO: order might be important here
     for match_type in matches.match_list:
+      if match_type.match_type not in requested_matchers:
+        continue
+      requested_matchers.remove(match_type.match_type)
+
       start = now()
       source_vectors = base_source_vectors.filter(type=match_type.vector_type)
       target_vectors = base_target_vectors.filter(type=match_type.vector_type)
@@ -56,9 +65,16 @@ def match(task_id):
               "{} matches".format(source_count, target_count, match_type,
                                   len(match_objs)))
         Match.objects.bulk_create(match_objs, batch_size=10000)
+      else:
+        print("Skipped matcher {} with {} local vectors and {} remote vectors"
+              "".format(match_type, source_count, target_count))
       print("\tTook: {}".format(now() - start))
 
       task.update(progress=F('progress') + 1)
+
+    if requested_matchers:
+      msg = "Unfamiliar matchers were requested: {}".format(requested_matchers)
+      raise ValueError(msg)
   except Exception:
     task.update(status=Task.STATUS_FAILED, finished=now())
     raise

pombredanne/rematch-1

server/collab/tasks.py
@@ -5,24 +5,29 @@ from collab import matchers
 
 from celery import shared_task
 
+import json
+
 
 @shared_task
 def match(task_id):
   try:
-    # recording the task has started
     task = Task.objects.filter(id=task_id)
-    task.update(status=Task.STATUS_STARTED, progress=0,
-                progress_max=len(matchers.matchers_list),
-                task_id=match.request.id)
 
     # get input parameters
     task_values = task.values_list('id', 'source_file_version__file_id',
                                    'source_start', 'source_end',
                                    'source_file_version_id',
-                                   'target_project_id', 'target_file_id').get()
+                                   'target_project_id', 'target_file_id',
+                                   'matchers').get()
     print(task_values)
     (task_id, source_file, source_start, source_end, source_file_version,
-     target_project, target_file) = task_values
+     target_project, target_file, requested_matchers) = task_values
+
+    requested_matchers = set(json.loads(requested_matchers))
+
+    # recording the task has started
+    task.update(status=Task.STATUS_STARTED, progress=0,
+                progress_max=len(requested_matchers), task_id=match.request.id)
 
     source_filter = {'file_id': source_file,
                      'file_version_id': source_file_version}
@@ -43,6 +48,10 @@ def match(task_id):
     print("Running task {}".format(match.request.id))
     # TODO: order might be important here
     for matcher in matchers.matchers_list:
+      if matcher.match_type not in requested_matchers:
+        continue
+      requested_matchers.remove(matcher.match_type)
+
       start = now()
       source_vectors = base_source_vectors.filter(type=matcher.vector_type)
       target_vectors = base_target_vectors.filter(type=matcher.vector_type)
@@ -56,9 +65,16 @@ def match(task_id):
               "{} matches".format(source_count, target_count, matcher,
                                   len(match_objs)))
         Match.objects.bulk_create(match_objs, batch_size=10000)
+      else:
+        print("Skipped matcher {} with {} local vectors and {} remote vectors"
+              "".format(matcher, source_count, target_count))
       print("\tTook: {}".format(now() - start))
 
       task.update(progress=F('progress') + 1)
+
+    if requested_matchers:
+      msg = "Unfamiliar matchers were requested: {}".format(requested_matchers)
+      raise ValueError(msg)
   except Exception:
     task.update(status=Task.STATUS_FAILED, finished=now())
     raise