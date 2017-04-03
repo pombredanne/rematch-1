d5218a43ee512a6c6023e97ec5e5cb2707f6745d
Nir Izraeli <nirizr@gmail.com>
2017-04-04 00:26:37 +0300

    test plugin basic interface
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- tests/idaplugin/test_plugin.py
+++ tests/idaplugin/test_plugin.py
@@ -2,4 +2,15 @@ from idaplugin import plugin_rematch
 
 
 def test_plugin_creation():
-  plugin_rematch.PLUGIN_ENTRY()
+  from PyQt5 import QtWidgets
+  qapp = QtWidgets.QApplication([])
+
+  plugin = plugin_rematch.PLUGIN_ENTRY()
+
+  plugin.init()
+
+  # TODO: validate return is of PLUGIN_* positive values
+
+  plugin.run()
+
+  plugin.term()
