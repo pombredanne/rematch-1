50c31c87aead12a76c7542268461429aef7229e9
Nir Izraeli <nirizr@gmail.com>
2017-04-05 15:54:30 +0300

    [WIP] refactor collectors to simplify, add annotation apply
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- idaplugin/rematch/collectors/collector.py
+++ idaplugin/rematch/collectors/collector.py
@@ -8,16 +8,7 @@ except NameError:
 
 
 class Collector(object):
-  def __init__(self, offset, instance_id=None):
-    self.instance_id = instance_id
-    self.offset = offset
-    self.data = self.serialized_data()
-
-  @staticmethod
-  def include():
-    return True
-
-  def serialized_data(self):
+  def data(self):
     data = self._data()
     if not isinstance(data, strtypes):
       data = json.dumps(data)
