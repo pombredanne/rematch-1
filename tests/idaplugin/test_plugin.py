d4dc9acbcef8bcb1938412371896309955c4be74
Nir Izraeli <nirizr@gmail.com>
2017-04-07 01:58:22 +0300

    Create an app thread, add main window
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- tests/idaplugin/test_plugin.py
+++ tests/idaplugin/test_plugin.py
@@ -1,9 +1,18 @@
 from idaplugin import plugin_rematch
 
+import threading
+
 
 def test_plugin_creation():
   from PyQt5 import QtWidgets
   qapp = QtWidgets.QApplication([])
+  qmainwin = QtWidgets.QMainWindow()
+  qmdiarea = QtWidgets.QMdiArea()
+  qmainwin.setCentralWidget(qmdiarea)
+  qmenu = QtWidgets.QMenu()
+  qmainwin.setMenuWidget(qmenu)
+  t = threading.Thread(target=qapp.exec_)
+  t.start()
 
   plugin = plugin_rematch.PLUGIN_ENTRY()
 
