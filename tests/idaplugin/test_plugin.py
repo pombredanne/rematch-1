78cd1933c0abd633a0a6daea9e48ca72bddb1206
Nir Izraeli <nirizr@gmail.com>
2017-04-09 18:19:38 +0200

    Remove local qapp creation in favor of pytest_idapro fixture
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- tests/idaplugin/test_plugin.py
+++ tests/idaplugin/test_plugin.py
@@ -1,19 +1,7 @@
 from idaplugin import plugin_rematch
 
-import threading
 
-
-def test_plugin_creation():
-  from PyQt5 import QtWidgets
-  qapp = QtWidgets.QApplication([])
-  qmainwin = QtWidgets.QMainWindow()
-  qmdiarea = QtWidgets.QMdiArea()
-  qmainwin.setCentralWidget(qmdiarea)
-  qmenu = QtWidgets.QMenu()
-  qmainwin.setMenuWidget(qmenu)
-  t = threading.Thread(target=qapp.exec_)
-  t.start()
-
+def test_plugin_creation(ida_app):
   plugin = plugin_rematch.PLUGIN_ENTRY()
 
   plugin.init()
