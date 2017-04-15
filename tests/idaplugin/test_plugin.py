bb2b2d2d51de5dc64c87b0916ad8ca7486f79fd1
Nir Izraeli <nirizr@gmail.com>
2017-04-15 22:16:17 +0300

    Rename pytest fixture ida_app to idaapp_pro
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- tests/idaplugin/test_plugin.py
+++ tests/idaplugin/test_plugin.py
@@ -1,4 +1,6 @@
-def test_plugin_creation(idapro_plugin_entry, ida_app):
+def test_plugin_creation(idapro_plugin_entry, idapro_app):
+  del idapro_app
+
   plugin = idapro_plugin_entry()
 
   plugin.init()
